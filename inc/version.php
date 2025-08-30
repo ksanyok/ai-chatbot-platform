de84458c40e766794ae534b85c0902f9f5b43473
Alexandr Krikun <44290517+ksanyok@users.noreply.github.com>
2025-08-30 07:33:03 -0700

    Add version.php for version control

---
+++ inc/version.php
@@ -0,0 +1,7 @@
+<?php
+// Application version definition
+// This file defines the current version of the AI chatbot platform.  When
+// releasing new versions, update the string below.  Other scripts can
+// include this file and compare with remote versions.
+
+const APP_VERSION = '0.3.0';
